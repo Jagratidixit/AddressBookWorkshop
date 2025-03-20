2afa5783b7576772834470d30acd4f694a9ad74a
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-20 13:08:37 +0530

    UC 1: Configure Database and Application Settings

---
+++ src/main/java/com/example/AddressBookWorkshop/model/Contact.java
@@ -0,0 +1,17 @@
+package com.example.AddressBookWorkshop.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+}
+
